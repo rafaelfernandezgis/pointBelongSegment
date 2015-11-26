6abc5104a72f9297c14348cc65b52558987f155a
Rafael <rafael.fernandez.gis@gmail.com>
2015-11-26 09:53:09 -0330

    Update isPointBelongSegment.js

--- isPointBelongSegment.js
+++ isPointBelongSegment.js
@@ -1,4 +1,4 @@
-export function isPointBelongSegment(Lx1, Ly1, Lx2, Ly2, Px, Py) {
+function isPointBelongSegment(Lx1, Ly1, Lx2, Ly2, Px, Py) {
     var ab = Math.sqrt(Math.pow((Lx2 - Lx1), 2) + Math.pow((Ly2 - Ly1), 2));
     var ac = Math.sqrt(Math.pow((Lx1 - Px), 2) + Math.pow((Ly1 - Py), 2));
     var bc = Math.sqrt(Math.pow((Lx2 - Px), 2) + Math.pow((Ly2 - Py), 2));
